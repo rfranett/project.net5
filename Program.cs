2bf4f599aa3becdb0e402a37a445aaf5d0aa694c
rfranett <rfranett@gmail.com>
2023-11-24 19:53:13 -0300

    operadoresc#

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using project.net5.models ;
+
+int a = 12 ;
+
+int b = 2 ;
+
+int c = a / b ;
+
+c = c * c ;
+
+
+
+/*
+int a = 10;
+
+int b = 25;
+
+int c = a + b;
+
+c += 5 ;  //  c = c + 5;
+*/
+
+
+
+/*ex2
+int a = 50 ;
+
+int b = 12 ;
+
+int c = a + b ;
+
+ c = c - 8 ;
+ */
+
+
+
+/* ex1
+
+int a = 32 ;
+
+int b = 55 ;
+
+int c = a + b ;
+*/
+
+Console.WriteLine(c);
